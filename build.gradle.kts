b6ccee920d924e8f9ceecc5547fdd79b0c31e5d3
Kerem <keremeniscakmak@gmail.com>
2026-06-14 19:36:48 -0400

    initial setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("io.github.ollama4j:ollama4j:1.1.7")
+    implementation("org.java-websocket:Java-WebSocket:1.5.7")
+    implementation("org.json:json:20240303")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 tasks.test {
